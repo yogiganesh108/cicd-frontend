45e9ce19d597ded35ee858a5f0156a1ce697446d
I.Y.G Nagendra <103441226+yogiganesh108@users.noreply.github.com>
2025-08-07 12:21:47 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -46,7 +46,7 @@ const BASE_URL = 'http://localhost:9090/springapp1';
     setIsEditing(true);
   };
 
-  /*
+  
   const deleteProduct = async (id) => {
     if (window.confirm('Are you sure you want to delete this product?')) {
       await axios.delete(`${BASE_URL}/delete/${id}`);
@@ -54,7 +54,7 @@ const BASE_URL = 'http://localhost:9090/springapp1';
       fetchProducts();
     }
   };
-  */
+  
 
   useEffect(() => {
     fetchProducts();
@@ -167,14 +167,14 @@ const BASE_URL = 'http://localhost:9090/springapp1';
                 >
                   Edit
                 </button>
-                {/*
+                
                 <button
                   className="btn btn-danger btn-sm"
                   onClick={() => deleteProduct(p.id)}
                 >
                   Delete
                 </button>
-                */}
+                
               </td>
             </tr>
           ))}
